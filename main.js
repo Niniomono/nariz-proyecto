a7eaeb645923a598448464bc538e6c43f4e5f4f5
Niniomono <97811640+Niniomono@users.noreply.github.com>
2022-10-27 18:57:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preLoad() {
+}
+function setup() {
+   canvas = createCanvas(300, 300);
+   canvas.center();
+}
+function draw() {
+}
+function take_snapshot() {
+  save('myFilterImage.PNG');
+}
